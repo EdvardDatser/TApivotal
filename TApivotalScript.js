7635a6f35dc350af35edb7c8ad3d7db821aa5bfc
EdvardDatser <120181268+EdvardDatser@users.noreply.github.com>
2024-05-02 00:10:59 +0300

    Vigade parandamine

--- TApivotalScript.js
+++ TApivotalScript.js
@@ -1,4 +1,4 @@
-
+// masiiv eesti sõnadega
 const sonad=[
     'teostus',
     'algorütm',
@@ -31,8 +31,7 @@ const sonad=[
     'arendamine',
     'valideerimine'
 ];
-
-
+// masiiv vene sõnadega
 const venesonad=[
     'реализация',
     'алгоритм',
@@ -66,24 +65,48 @@ const venesonad=[
     'валидрование'
 ];
 function randomSonad(){
-
     //random sõnad - arv
     const juhuslikSona = Math.floor(Math.random()*sonad.length);
-
     //võtme random sõna massivist
     const sona = sonad[juhuslikSona];
-
     //lisame HTML lehele
-
     document.getElementById("random-sona").innerHTML=sona;
 }
-
 function kontrollima() {
-
+    // võtme HTML lehele elementid
+    const sona = document.getElementById("random-sona").innerText;
+    const venesona = document.getElementById("kontroll").value;
+    // saime indeksid nendest elemendi
+    const indEesti = sonad.indexOf(sona);
+    const indVene = venesonad.indexOf(venesona);
+    // Valideerimine
     if (indEesti === indVene && indEesti !== -1 && indVene !== -1) {
         document.getElementById("vastus").innerHTML = "Õige vastus!";
     } else {
         document.getElementById("vastus").innerHTML = "Vale vastus(((";
     }
+}
+function randomSonadvene(){
+    //random sõnad - arv
+    const juhuslikSona = Math.floor(Math.random()*venesonad.length);
+    //võtme random sõna massivist
+    const sona = venesonad[juhuslikSona];
+    //lisame HTML lehele
+    document.getElementById("random-sonavene").innerHTML=sona;
+}
+function kontrollimavene() {
+    // võtme HTML lehele elementid
+    const sona = document.getElementById("random-sonavene").innerText;
+    const eestisona = document.getElementById("kontrollvene").value;
 
+    // saime indeksid nendest elemendi
+    const indVene = venesonad.indexOf(sona);
+    const indEesti = sonad.indexOf(eestisona);
+
+    // Valideerimine
+    if (indVene === indEesti && indVene !== -1 && indEesti !== -1) {
+        document.getElementById("vastusvene").innerHTML = "Õige vastus!";
+    } else {
+        document.getElementById("vastusvene").innerHTML = "Vale vastus(((";
+    }
 }
